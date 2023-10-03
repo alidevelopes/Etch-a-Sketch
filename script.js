fd3533a78f56d66744c67b7f911118546ee3ba7d
alidevelopes <alidevelopes314@gmail.com>
2023-10-03 18:20:47 +0200

    Initial commit - add html, css and js file.

---
+++ script.js
@@ -0,0 +1,8 @@
+// ui
+// targeting grid-container div
+const gridContainer = document.querySelector(".grid-container");
+
+const gridDiv = document.createElement("div");
+// gridDiv.setAttribute("class");
+gridDiv.classList.add("grid-div");
+gridContainer.appendChild(gridDiv);
