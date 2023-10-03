06b33f7528c1ab42f4cefdd5d08ff6f5bbde07f9
alidevelopes <alidevelopes314@gmail.com>
2023-10-04 01:32:46 +0200

    Finish html and css layout, and add a function.
    html & css part: most of the layout & positioning is done, some minor changes will be made when necessary. Font sizing & colors are yet to be fixed.
    
    js part: createGrid(n) function is added which creates n by n number of squares inside a fixed sized container div.

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // ui
 // targeting grid-container div
+
+const gridContainerSize = 550;
+
+let numOfGrids = 1;
+
 const gridContainer = document.querySelector(".grid-container");
 
-const gridDiv = document.createElement("div");
-// gridDiv.setAttribute("class");
-gridDiv.classList.add("grid-div");
-gridContainer.appendChild(gridDiv);
+function createGrid(n) {
+  numOfGrids = n * n;
+  let gridWidth = 550 / n;
+  let gridHeight = 550 / n;
+  for (let i = 1; i <= numOfGrids; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add(`grid`);
+    gridDiv.setAttribute("id", `gridNum${i}`);
+    gridDiv.style.width = `${gridWidth}px`;
+    gridDiv.style.height = `${gridHeight}px`;
+    gridDiv.textContent = i;
+
+    // adding width and height
+    gridContainer.appendChild(gridDiv);
+  }
+}
+
+createGrid(4);
