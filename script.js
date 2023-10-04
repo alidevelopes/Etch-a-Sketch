653bceb6409c79eed8a1b199267e20047a2b51f7
alidevelopes <alidevelopes314@gmail.com>
2023-10-04 20:20:47 +0200

    Layout, designing and UI is almost done.
    HTML & CSS part: fonts and new colors are added to whole body.
    JavaScript part: all buttons (except rainbow button), including color pickers and slider is now working.
    
    Next step is to add a small display text to show grid number like "10 x 10" when user uses slider. Also to add functionality to "Rainbow" button.

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-// ui
-// targeting grid-container div
+//
+//
 
 const gridContainerSize = 550;
-
-let numOfGrids = 1;
-
 const gridContainer = document.querySelector(".grid-container");
 
+let totalNumOfGrids = 1;
+let gridInputValue = 1;
+let penColor = "";
+let sheetColor = "";
+const SLIDER_DEFAULT_VALUE = 10;
+const PEN_DEFAULT_COLOR = "#000000";
+const SHEET_DEFAULT_COLOR = "#ffffff";
+penColor = PEN_DEFAULT_COLOR;
+sheetColor = SHEET_DEFAULT_COLOR;
+
+const penClr = document.getElementById("pen");
+const sheetClr = document.getElementById("sheet");
+const slider = document.getElementById("grid-slider");
+const eraseBtn = document.querySelector(".erase");
+const clearBtn = document.querySelector(".clear");
+const resetBtn = document.querySelector(".reset");
+const penClrPickerContainer = document.querySelector(".clr-picker-1");
+const sheetClrPickerContainer = document.querySelector(".clr-picker-2");
+
+resetBtn.addEventListener("click", () => {
+  gridContainer.textContent = "";
+  n = SLIDER_DEFAULT_VALUE;
+  penColor = PEN_DEFAULT_COLOR;
+  sheetColor = SHEET_DEFAULT_COLOR;
+  gridContainer.style.backgroundColor = SHEET_DEFAULT_COLOR;
+  penClrPickerContainer.style.backgroundColor = penColor;
+  sheetClrPickerContainer.style.backgroundColor = sheetColor;
+  createGrid(n);
+});
+
+eraseBtn.addEventListener("click", () => (penColor = sheetColor));
+penClr.addEventListener("input", (event) => {
+  penColor = event.target.value;
+  penClrPickerContainer.style.backgroundColor = penColor;
+});
+
+sheetClr.addEventListener("input", (event) => {
+  sheetColor = event.target.value;
+  gridContainer.style.backgroundColor = sheetColor;
+  sheetClrPickerContainer.style.backgroundColor = sheetColor;
+});
+
+slider.addEventListener("click", (event) => {
+  if (gridContainer.firstChild) gridContainer.textContent = "";
+  gridInputValue = Math.floor(event.target.value);
+  n = gridInputValue;
+  createGrid(n);
+});
+
+clearBtn.addEventListener("click", () => {
+  gridContainer.textContent = "";
+  penColor = SHEET_DEFAULT_COLOR;
+  n = SLIDER_DEFAULT_VALUE;
+  createGrid(n);
+});
+
+// create grids (squares) and add event listeners to each grid
 function createGrid(n) {
-  numOfGrids = n * n;
+  totalNumOfGrids = n * n;
   let gridWidth = 550 / n;
   let gridHeight = 550 / n;
-  for (let i = 1; i <= numOfGrids; i++) {
+
+  for (let i = 1; i <= totalNumOfGrids; i++) {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add(`grid`);
     gridDiv.setAttribute("id", `gridNum${i}`);
     gridDiv.style.width = `${gridWidth}px`;
     gridDiv.style.height = `${gridHeight}px`;
-    gridDiv.textContent = i;
-
-    // adding width and height
     gridContainer.appendChild(gridDiv);
+
+    // adding event listener to each grid
+    const gridX = document.getElementById(`gridNum${i}`);
+    gridX.addEventListener("click", function () {
+      gridX.style.backgroundColor = `${penColor}`;
+    });
   }
 }
 
-createGrid(4);
+createGrid(SLIDER_DEFAULT_VALUE);
